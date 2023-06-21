bdf2fac9791b62d6a7a3a437fe9cdda9bca361e3
Farhan Khorsheed <ratul097@gmail.com>
2023-06-22 00:38:45 +1000

    ADDED template and installed initial dependencies

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import { Figtree } from 'next/font/google'
+
+import Sidebar from '@/components/Sidebar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to Music',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Handle logout in the future
+  };
+
+  return <div></div>;
+};
+
+export default Header;
